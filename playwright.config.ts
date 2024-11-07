53f8f666ebaf6d6a7bfe759f0ef1355a27519f65
josiashrojas <josiashrojas@gmail.com>
2024-11-07 18:59:01 -0300

    fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
