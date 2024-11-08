a70a6aa8f2ab9479437f87b537c85c64eee87a8b
josiashrojas <josiashrojas@gmail.com>
2024-11-08 16:21:30 -0300

    save report always

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     baseURL: 'http://localhost:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: testEnv.ci ? 'on' : 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
