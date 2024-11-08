5f42bc07bb1ad610ae3f340e0e9e5385ca99d400
josiashrojas <josiashrojas@gmail.com>
2024-11-08 15:41:49 -0300

    use env variables test

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 test('get started link', async ({ page }) => {
   await page.goto('/');
 
+  console.log(process.env["ENV"]);
+
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: '2 Hello 2, test-ci' })).toBeVisible();
 });
