9abc182ea4849ffb74734b87437332073de45cc1
josiashrojas <josiashrojas@gmail.com>
2024-11-13 10:24:44 -0300

    testing with real case

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -14,3 +14,12 @@ test('flaky test', async ({ page }, testInfo) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name, exact: true })).toBeVisible();
 });
+
+test('browser', async ({page}) => {
+  test.setTimeout(50 * 1000);
+  await page.goto('https://workspace-workspace.haulmer.dev');
+  await page.getByLabel('Email').fill('usuario_test01@haulmer.net');
+  await page.getByLabel('Contraseña').fill('Pass1234');
+  await page.getByRole('button', { name: 'iniciar', exact: true }).click();
+  await expect(page.locator('app-homepage-initial')).toBeVisible({timeout: 20 * 1000});
+})
